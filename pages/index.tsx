90197a4f9954e2755363e78dfad3eb849b0f5d5a
Miguel Penha <miguel.penha1@gmail.com>
2023-07-20 20:37:53 -0300

    Teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { IQuestions } from '../types'
 import useEvaluateGlasses from '../components/useEvaluateGlasses'
 import Head from 'next/head'
 import { Container, Title } from '../styles/pages'
+import Button from '../components/Button'
 import FormQuestions from '../components/Form'
 import GlassesList from '../components/GlassesList'
 
@@ -16,7 +17,15 @@ function Home() {
             <title>Ótica MVP</title>
         </Head>
         <Container>
-            <Title>{showGlasses ? 'As melhores opções baseada nas respostas' : 'Faça uma avaliação'}</Title>
+            <Title showGlasses={showGlasses}>{showGlasses ? 'As melhores opções baseada nas respostas' : 'Faça uma avaliação'}</Title>
+            {showGlasses && (
+                <Button title="Refazer" onClick={() => location.reload()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
+                    </svg>
+                </Button>
+            )}
             <FormQuestions setQuestions={setQuestions} onFinish={() => setShowGlasses(true)}/>
             {showGlasses && (
                 <GlassesList glasses={glasses}/>
--- styles/pages/index.ts
+++ styles/pages/index.ts
@@ -5,11 +5,16 @@ export const Container = styled.main`
     flex-direction: column;
 `
 
-export const Title = styled.h1`
+interface ITitle {
+    showGlasses: boolean
+}
+
+export const Title = styled.h1<ITitle>`
     margin-top: 6vh;
     font-size: 2.2em;
     align-self: center;
     text-align: center;
+    margin-bottom: ${props => props.showGlasses ? '1.5' : '0'}em;
 
     @media screen and (max-width: 900px) {
         font-size: 1.5em;
