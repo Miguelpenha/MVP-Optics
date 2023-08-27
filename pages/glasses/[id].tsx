ae0f5fc819dea0555b0443811cf382c8f4ebe32b
Miguel Penha <miguel.penha1@gmail.com>
2023-08-27 18:10:49 -0300

    Update [id].tsx

--- pages/glasses/[id].tsx
+++ pages/glasses/[id].tsx
@@ -1,44 +1,46 @@
-import { useRouter } from 'next/router'
-import useGlasses from '../../services/useGlasses'
+import { IGlasses } from '../../types'
+import { FC } from 'react'
 import Head from 'next/head'
 import { Container, Title, Image } from '../../styles/pages/glasses'
 import ButtonBack from '../../components/ButtonBack'
 import blurData from '../../utils/blurData'
+import { GetServerSideProps } from 'next'
+import useGlasses from '../../services/useGlasses'
 
-interface IQuery {
-    id: string
+interface IProps {
+    glasses: IGlasses
 }
 
-function Glasses() {
-    const { id } = useRouter().query as unknown as IQuery
-    const glasses = useGlasses(id)
+const Glasses: FC<IProps> = ({ glasses }) => {
+    return <>
+        <Head>
+            <title>{glasses.name}</title>
+            <meta property="og:title" content={glasses.name}/>
+            <meta property="og:image" content={glasses.image}/>
+            <meta name="description" content={glasses.description}/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="og:description" content={glasses.description}/>
+            <meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN}/glasses/${glasses.id}`}/>
+        </Head>
+        <Container>
+            <ButtonBack back="/"/>
+            <Title>{glasses.name}</Title>
+            <Image
+                fill
+                placeholder="blur"
+                src={glasses.image}
+                blurDataURL={blurData}
+                alt={glasses.description}
+            />
+        </Container>
+    </>
+}
 
-    if (glasses) {
-        return <>
-            <Head>
-                <title>{glasses.name}</title>
-                <meta property="og:title" content={glasses.name}/>
-                <meta property="og:image" content={glasses.image}/>
-                <meta name="description" content={glasses.description}/>
-                <meta name="twitter:card" content="summary_large_image"/>
-                <meta property="og:description" content={glasses.description}/>
-                <meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN}/glasses/${glasses.id}`}/>
-            </Head>
-            <Container>
-                <ButtonBack back="/"/>
-                <Title>{glasses.name}</Title>
-                <Image
-                    fill
-                    placeholder="blur"
-                    src={glasses.image}
-                    blurDataURL={blurData}
-                    alt={glasses.description}
-                />
-            </Container>
-        </>
-    } else {
-        return null
-    }
+export const getServerSideProps: GetServerSideProps<IProps> = async (req) => {
+    const { id } = req.query
+    const glasses: IGlasses = useGlasses(id as string)
+
+    return { props: { glasses } }
 }
 
 export default Glasses
